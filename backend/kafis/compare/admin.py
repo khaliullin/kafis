03cc8686183741eb6e39ec214ded72f03ba4c4b5
khaliullin <sgtkaliullin@gmail.com>
2019-12-08 00:06:45 +0300

    admin person search field added

--- backend/kafis/compare/admin.py
+++ backend/kafis/compare/admin.py
@@ -2,7 +2,12 @@ from django.contrib import admin
 
 from compare.models import Person, Compare, Expert, Report
 
-admin.site.register(Person)
+
+class PersonAdmin(admin.ModelAdmin):
+    search_fields = ['name', 'id', 'vk_id']
+
+
+admin.site.register(Person, PersonAdmin)
 admin.site.register(Expert)
 admin.site.register(Compare)
 admin.site.register(Report)
